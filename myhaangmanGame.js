92fdce311b8bd027ed8401a81fb5d363b6d843f5
nikki763 <68808863+nikki763@users.noreply.github.com>
2020-08-25 22:05:37 +0530

    added files
    added js and css

---
+++ myhaangmanGame.js
@@ -0,0 +1,11 @@
+
+	var name=prompt("Enter your name to play");
+	$("#demo").text("HEYY, "+name);
+	alert("let's play the game");
+	$("#demo1").slideUp(1000).slideDown(2000);//animation
+	function time()
+	{
+		alert("OOPS TIMEUP!!!");
+	}
+
+
